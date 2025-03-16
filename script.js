8129d084759d7ac8b7e85a12c60a385127748fdb
Khalidul Islam Rudro <khalidulislam.rudro@gmail.com>
2025-03-16 23:09:47 +0600

    add card section

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ function loadCategory(){
     .then((data) =>  displayCategories(data.categories))
 }
 
+
+function loadVideos(){
+    fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((response)=> response.json())
+    .then((data) =>  displayVideos(data.videos))
+}
+
 function displayCategories (categories){
     const categoryContainer = document.getElementById('category-container')
     for (let cat of categories){
@@ -17,4 +24,32 @@ function displayCategories (categories){
     
 }
 
+const displayVideos =(videos) => {
+    const videoContainer = document.getElementById("video-container")
+
+    videos.forEach((videos) => {
+        const videoCard = document.createElement("div")
+        videoCard.innerHTML = `
+        <div class="bg-base-100  shadow-sm">
+  <figure>
+    <img
+      src="${videos.thumbnail}"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="text-xl">${videos.title} </h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+        
+        `
+        videoContainer.append(videoCard)
+    });
+}
+
 loadCategory()
+
+loadVideos()
